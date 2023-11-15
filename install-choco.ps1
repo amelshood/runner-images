12d194ded1688e838a9bfa3408c832c11164acfa
amelshood <51880920+amelshood@users.noreply.github.com>
2023-11-15 10:25:28 -0500

    Update install-choco.ps1

--- install-choco.ps1
+++ install-choco.ps1
@@ -7,3 +7,16 @@ powershell -executionpolicy Bypass -File "$env:TEMP\installChocolatey.ps1"
 
 # Remove downloaded script
 Remove-Item "$env:TEMP\installChocolatey.ps1" -Force
+
+# 2
+# Import-Module 'C:\Chocolatey\Scripts\install.ps1'
+# Install-Chocolatey -y
+
+# 3. Create a new batch file named installChocolatey.cmd and add the following content:
+# @echo off
+# cd /temp
+# powershell -ExecutionPolicy Bypass -Command "(New-Object System.Net.WebClient).DownloadFile('https://community.chocolatey.org/install.ps1', 'installChocolatey.ps1')"
+# powershell -ExecutionPolicy Bypass -File '.\installChocolatey.ps1'
+# set PATH=%PATH%;%ChocolateyInstall%\bin
+# choco upgrade -y chocolatey
+
